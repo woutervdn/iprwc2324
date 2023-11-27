b6d0dda080ca5323f520f33e58fab9ef6cee00fa
Wouter <s1127784@student.hsleiden.nl>
2023-11-27 12:46:37 +0100

    init

---
+++ src/main/java/nl/hsleiden/iprwc2324/Iprwc2324Application.java
@@ -0,0 +1,13 @@
+package nl.hsleiden.iprwc2324;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iprwc2324Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Iprwc2324Application.class, args);
+    }
+
+}
---
+++ src/test/java/nl/hsleiden/iprwc2324/Iprwc2324ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hsleiden.iprwc2324;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iprwc2324ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
