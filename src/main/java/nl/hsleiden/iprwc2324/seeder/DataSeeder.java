fa16ee910c3542ef57e2a30d25a351c66687b6a6
Wouter <s1127784@student.hsleiden.nl>
2024-03-14 15:38:19 +0100

    added products to seeder and fixed admin ui

--- src/main/java/nl/hsleiden/iprwc2324/seeder/DataSeeder.java
+++ src/main/java/nl/hsleiden/iprwc2324/seeder/DataSeeder.java
@@ -61,8 +61,11 @@ public class DataSeeder implements CommandLineRunner {
         Product prod4 = new Product("Trui", "/assets/sweater.jpeg", BigDecimal.valueOf(49.99), "Dikke trui voor jongeren", heren.getId());
         Product prod5 = new Product("Zomer Jurk", "/assets/dress.jpg", BigDecimal.valueOf(32.99), "Luchtige zomer jurk voor het strand", dames.getId());
         Product prod6 = new Product("Winterjas", "/assets/coat.jpg", BigDecimal.valueOf(44.99), "Bruine modieuze winterjas voor mannen", actie.getId());
-        Product prod7 = new Product("Overall", "/assets/overall.jpg", BigDecimal.valueOf(44.99), "Bruine modieuze winterjas voor mannen", actie.getId());
-        productRepository.saveAll(Arrays.asList(prod1, prod2, prod3, prod4, prod5, prod6));
+        Product prod7 = new Product("Overall", "/assets/overall.jpg", BigDecimal.valueOf(54.99), "Bruine stoffen overall", actie.getId());
+        Product prod8 = new Product("Cargo Broek", "/assets/cargo.jpg", BigDecimal.valueOf(44.99), "Zwarte syntetische cargo broek", heren.getId());
+        Product prod9 = new Product("Bruine Zonnebril", "/assets/sunglasses.jpg", BigDecimal.valueOf(19.99), "Bruine stylistische zonnebril", dames.getId());
+        Product prod10 = new Product("Wit Overhemd", "/assets/white_shirt.jpg", BigDecimal.valueOf(79.99), "Professioneel wit overhemd", heren.getId());
+        productRepository.saveAll(Arrays.asList(prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8, prod9, prod10));
 
         ProductOrderItem item1 = new ProductOrderItem(prod1, 2);
         ProductOrderItem item2 = new ProductOrderItem(prod2, 3);
